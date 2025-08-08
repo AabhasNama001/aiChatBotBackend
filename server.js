ae7e0b33cf92338091f8b3de4e85167fe20cb27e
Aabhas Nama <aabhasa9@gmail.com>
2025-08-08 18:04:01 +0530

    deployed frontend url added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://ai-chat-bot-frontend-eight.vercel.app/",
   },
 });
 
